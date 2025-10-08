8bf9facf5ad6b88958cf2d75cfbb83707b9e93f0
Hasan Alhajhamad <hzhamad@gmail.com>
2025-10-09 01:33:42 +0300

    feat: Revoke user token on logout to enhance session security

--- app/Http/Controllers/Web/AuthController.php
+++ app/Http/Controllers/Web/AuthController.php
@@ -252,6 +252,7 @@ class AuthController extends Controller
 
         $request->session()->invalidate();
         $request->session()->regenerateToken();
+        $request->user()->token()->revoke();
 
         return redirect()->route('login');
     }
