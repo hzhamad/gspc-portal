d8c45b572a6dd3b74b71832811e694001a46cc30
Hasan Alhajhamad <hzhamad@gmail.com>
2025-10-10 00:39:27 +0300

    feat: add Markdown email rendering support with customizable themes and components

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,16 @@
+@props(['url'])
+<tr>
+    <td class="header">
+        <a href="{{ $url }}" style="display: inline-block;">
+            @if (trim($slot) === 'Laravel')
+            <img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
+            @else
+            @if(isset($logo) && $logo)
+            <img src="{{ $logo }}" class="logo" alt="{{ config('app.name') }} Logo">
+            @else
+            {!! $slot !!}
+            @endif
+            @endif
+        </a>
+    </td>
+</tr>
